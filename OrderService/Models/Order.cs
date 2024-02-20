929be4315231f30f63b2b3f1a8e72aa888a3f2a7
hclee <lhc0724@gmail.com>
2024-02-20 17:29:50 +0900

    clean code

--- OrderService/Models/Order.cs
+++ OrderService/Models/Order.cs
@@ -1,13 +1,12 @@
-using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations.Schema;
+using OrderService.Models;
 
-namespace OrdersService.Models
+namespace OrdersService.Models;
+
+public class Order
 {
-    public class Order
-    {
-        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-        public long Id { get; set; }
-        public string Status { get; set; }
-        public virtual List<OrderLine> Lines { get; set; }
-    }
+  [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+  public long Id { get; set; }
+  public string Status { get; set; }
+  public virtual List<OrderLine> Lines { get; set; }
 }
--- OrderService/Models/OrderLine.cs
+++ OrderService/Models/OrderLine.cs
@@ -1,18 +1,18 @@
 using System.ComponentModel.DataAnnotations.Schema;
+using OrdersService.Models;
 
-namespace OrdersService.Models
+namespace OrderService.Models;
+
+public class OrderLine
 {
-    public class OrderLine
-    {
-        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-        public long Id { get; set; }
-        public long Product_Id { get; set; }
-        public int Quantity { get; set; }
-        public decimal UnitPrice { get; set; }
-        public decimal TotalPrice => UnitPrice * Quantity;
-        
-        [ForeignKey(nameof(Order))]
-        public long Order_Id { get; set; }
-        public virtual Order Order { get; set; }
-    }
+	[DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+	public long Id { get; set; }
+	public long Product_Id { get; set; }
+	public int Quantity { get; set; }
+	public decimal UnitPrice { get; set; }
+	public decimal TotalPrice => UnitPrice * Quantity;
+
+	[ForeignKey(nameof(Order))]
+	public long Order_Id { get; set; }
+	public virtual Order Order { get; set; }
 }
